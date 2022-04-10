0d993bd8c3885a4401f8b1e843fb7bc4dd8a5d14
AnnSinger <101496144+AnnSinger@users.noreply.github.com>
2022-04-10 13:53:42 +0400

    Settings were added

--- main.py
+++ main.py
@@ -1,43 +1,26 @@
-heroes_health = 1000
-print('Здоровье Героя = ' + str(heroes_health))
-heroes_dmg = 100
-print('Урон Героя = ' + str(heroes_dmg))
-titan_health = 100
-print('Здоровье Титана = ' + str(titan_health))
-titan_dmg = 30
-print('Урон Титана = ' + str(titan_dmg))
-titan_helper_health = 60
-print('Здоровье помощника Титана = ' + str(titan_health))
-titan_helper_dmg = 20
-print('Урон помощьника Титана = ' + str(titan_helper_dmg))
+#Settings
+hero_name = input('Введите Имя Героя ')
+hero_health = int(input('Введите Здоровье Героя '))
+hero_dmg = int(input('Введите Урон Героя '))
+
+list_of_allies = []
+list_of_enemies = []
 
 while True:
-    if heroes_health < 0:
-        print('Ваш герой мертв')
+    command = input('Введите команду ')
+    if command == 'союзник':
+        ally_name = input('Введите Имя Союзник ')
+        ally_health = int(input('Введите Здоровье Союзника '))
+        ally_dmg = int(input('Введите Урон Союзника '))
+        list_of_allies.append([ally_name, ally_health, ally_dmg])
+    elif command == 'враг':
+        enemy_name = input('Введите Имя Врага ')
+        enemy_health = int(input('Введите Здоровье Врага '))
+        enemy_dmg = int(input('Введите Урон Врага '))
+        list_of_enemies.append([enemy_name, enemy_health, enemy_dmg])
+    elif command == 'выход':
         break
-    if titan_health < 0:
-        print('Титан мертв')
-    if titan_health < 0:
-        print('Помощник Титана мертв')
-
-    user_input = input('Кого будете атаковать? ')
-
-    if user_input == 'Titan':
-        titan_health = titan_health - heroes_dmg
-        print('Здоровье Титана = ' + str(titan_health))
-    elif user_input == 'Titan helper':
-        titan_helper_health = titan_helper_health - heroes_dmg
-        print('Здоровье помощника Титана = ' + str(titan_helper_health))
     else:
-        print('Неправильно введено имя')
-        user_input = input('Кого будете атаковать? ')
-
-    if titan_health > 0:
-        heroes_health = heroes_health - titan_dmg
-        print('Секира Титана проносится над головой Героя')
-        print('Здоровье Героя = ' + str(heroes_health))
-
-    if titan_helper_health > 0:
-        heroes_health = heroes_health - titan_helper_dmg
-        print('Помощник Титана коварно бьет вас в спину')
-        print('Здоровье Героя = ' + str(heroes_health))
+        print('Нет такой команды')
+print(list_of_allies)
+print(list_of_enemies)
