6b3904e3895892eec30f9090dba7b50586eb9556
AnnSinger <101496144+AnnSinger@users.noreply.github.com>
2022-04-18 11:21:31 +0400

    Update main.py
    Gameplay was added

--- main.py
+++ main.py
@@ -22,5 +22,46 @@ while True:
         break
     else:
         print('Нет такой команды')
+
+print(hero_name, hero_health, hero_dmg)
 print(list_of_allies)
 print(list_of_enemies)
+
+#Gameplay
+print('Вы в игре')
+while True:
+    if hero_health < 0:
+        print('Ваш герой мертв')
+        break
+
+    if len(list_of_allies) == 0:
+        print('Ваши союзники мертвы')
+        break
+
+    if len(list_of_enemies) == 0:
+        print('Ваши противники мертвы')
+        break
+
+        print(list_of_allies)
+        print(list_of_enemies)
+
+    command = int(input('Кого будете атаковать? '))
+    if command >= 0 and command < len(list_of_enemies):
+        list_of_enemies[command][1] -= hero_dmg
+        print('В результате атаки были нанесены удары по ' + str(list_of_enemies[0][0]) + '. У него осталось ' + str(list_of_enemies[0][1]) + ' здоровья')
+        if list_of_enemies[command][1] <= 0:
+            list_of_enemies.pop(command)
+
+    for ally_elem in list_of_allies:
+        list_of_enemies[0][1] -= ally_elem[2]
+
+    print('Удар пришёлся по врагу ' + str(list_of_enemies[0][0]) + 'У него осталось ' + str(list_of_enemies[0][1]) + ' здоровья')
+    if list_of_enemies[0][1] <= 0:
+        list_of_enemies.pop(0)
+
+    for enemy_elem in list_of_enemies:
+        list_of_allies[0][1] -= enemy_elem[2]
+
+    print('Удар пришёлся по союзнику ' + str(list_of_allies[0][0]) + 'У него осталось ' + str(list_of_allies[0][1]) + ' здоровья')
+    if list_of_allies[0][1] <= 0:
+        list_of_allies.pop(0)
